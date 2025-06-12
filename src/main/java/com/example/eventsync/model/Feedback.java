277797afe9c949161a5c051f6290c08c52520c9d
Kšyštof Tužik <krzysztof2003t@gmail.com>
2025-06-12 12:29:31 +0300

    feat: add Feedback entity model

---
+++ src/main/java/com/example/eventsync/model/Feedback.java
@@ -0,0 +1,16 @@
+package com.example.eventsync.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
+@Builder
+public class Feedback {
+    private UUID id;
+    private UUID eventId;
+    private String message;
+    private Instant createdAt;
+}
